f47c1f80984aa987e4281047377efc0fd5617b49
wazab <miroslavprokashev@mail.ru>
2023-09-17 19:45:17 +0300

    add file

--- app_advertisment/admin.py
+++ app_advertisment/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from .models import Advertisement
+import datetime
 
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'text', 'author', 'auction', 'price']
+    list_display = ['id', 'title', 'text', 'author', 'auction', 'price', 'get_updated_at_display']
     list_filter = ['author', 'title']
     actions = ['delete_description', 'make_auction_as_true']
+
+    def get_updated_at_display(self, obj):
+        if obj.update_at.date() == datetime.date.today():
+            return f'<Сегодня в {obj.update_at.strftime("%H:%M")}>'
+        else:
+            return obj.update_at.strftime("%Y-%m-%d %H:%M")
+
+    get_updated_at_display.short_description = 'Последнее обновление'
+    get_updated_at_display.allow_tags = True
 
     @admin.action(description="Удалить описание выбранных объектов")
     def delete_description(self, request, queryset):
